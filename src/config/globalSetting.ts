9c4c4ff61a9c1907926c446c00acf065901142ba
Моривец Владимир <morivvv@gmail.com>
2024-09-04 10:37:09 +0400

    обновление имени пакета

--- src/config/globalSetting.ts
+++ src/config/globalSetting.ts
@@ -3,5 +3,4 @@ export const defaultSchema = process.env.DEFAULT_POSTGRES_SCHEMA || "public";
 export const accessTable = process.env.ACCESS_TABLE || [
   "pg_catalog",
   "information_schema",
-  "seodo",
 ];
