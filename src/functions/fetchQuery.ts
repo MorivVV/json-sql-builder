c2410f32b29ad177bd6b3ba4769f5d29241ffc1d
Моривец Владимир <morivvv@gmail.com>
2024-09-13 23:51:16 +0400

    функция для запроса

---
+++ src/functions/fetchQuery.ts
@@ -0,0 +1,31 @@
+import { IJMQL } from "../types/restApi";
+type ArrayType<T> = T extends readonly [...infer Item] ? Item[number] : T;
+
+export const mqlFetchQuery = async <T extends Record<"table", string>>(
+  mql: IJMQL<T>,
+  url: string,
+  token = ""
+): Promise<Pick<T, ArrayType<IJMQL<T>["fields"]>>> => {
+  const data = JSON.stringify(mql);
+  const conf: RequestInit = {
+    body: data,
+    credentials: "same-origin",
+    method: "POST",
+    headers: {
+      "V-Token": token,
+      "Content-Type": "application/json;charset=utf-8",
+    },
+  };
+
+  return fetch(url, conf).then(async (response) => {
+    let restext: any = "";
+    try {
+      restext = await response
+        .json()
+        .catch(() => ({ err: { code: response.status } }));
+    } catch (error) {
+      restext = await response.text();
+    }
+    return restext;
+  });
+};
