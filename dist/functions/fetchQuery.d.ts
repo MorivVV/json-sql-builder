e11a1901880e214c14e74a743742b1273aa4efe9
Моривец Владимир <morivvv@gmail.com>
2024-09-14 00:05:38 +0400

    компиляция

--- dist/functions/fetchQuery.d.ts
+++ dist/functions/fetchQuery.d.ts
@@ -1,2 +1,4 @@
 import { IJMQL } from "../types/restApi";
-export declare const mqlFetchQuery: <T extends Record<"table", string>>(mql: IJMQL<T>, url: string, token?: string) => Promise<T[]>;
+export declare const mqlFetchQuery: <T extends Record<string, any> & {
+    table: string;
+}>(mql: IJMQL<T>, url: string, token?: string) => Promise<T[]>;
