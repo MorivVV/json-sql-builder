82eed09d4267bb3a23d588e28035da977e9e559e
Моривец Владимир <morivvv@gmail.com>
2024-09-17 22:50:30 +0400

    без дочерних классов

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,10 +1,3 @@
 export type { IJMQL, IAliasTableFields, ICreateTableFields, TMergeTInterface, IRestGet, IrestDelete, IrestInsert, IrestUpdate, } from "./types/restApi";
-import { JoinTables } from "./queryBuilder/JoinTables";
-import { SelectFields } from "./queryBuilder/SelectFields";
-import { UpdateFields } from "./queryBuilder/UpdateFields";
-import { WhereFilter } from "./queryBuilder/WhereFilter";
-import { Query } from "./creators/classQuery";
-import { BasicQuery } from "./queryBuilder/BasicQuery";
-import { FromTables } from "./queryBuilder/FromTables";
-import { mqlFetchQuery } from "./functions/fetchQuery";
-export { JoinTables, SelectFields, UpdateFields, WhereFilter, Query, BasicQuery, FromTables, mqlFetchQuery, };
+export { Query } from "./creators/classQuery";
+export { mqlFetchQuery } from "./functions/fetchQuery";
--- src/index.ts
+++ src/index.ts
@@ -8,23 +8,6 @@ export type {
   IrestInsert,
   IrestUpdate,
 } from "./types/restApi";
-import { JoinTables } from "./queryBuilder/JoinTables";
-import { SelectFields } from "./queryBuilder/SelectFields";
-import { UpdateFields } from "./queryBuilder/UpdateFields";
-import { WhereFilter } from "./queryBuilder/WhereFilter";
-import { Query } from "./creators/classQuery";
-import { BasicQuery } from "./queryBuilder/BasicQuery";
-import { FromTables } from "./queryBuilder/FromTables";
+export { Query } from "./creators/classQuery";
 
-import { mqlFetchQuery } from "./functions/fetchQuery";
-
-export {
-  JoinTables,
-  SelectFields,
-  UpdateFields,
-  WhereFilter,
-  Query,
-  BasicQuery,
-  FromTables,
-  mqlFetchQuery,
-};
+export { mqlFetchQuery } from "./functions/fetchQuery";
