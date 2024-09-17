be32f2f10a63d39ef7854135de91961542f3ebd4
Моривец Владимир <morivvv@gmail.com>
2024-09-17 22:34:18 +0400

    правки экспорта

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,9 +1,10 @@
 export type { IJMQL, IAliasTableFields, ICreateTableFields, TMergeTInterface, IRestGet, IrestDelete, IrestInsert, IrestUpdate, } from "./types/restApi";
-export { JoinTables } from "./queryBuilder/JoinTables";
-export { SelectFields } from "./queryBuilder/SelectFields";
-export { UpdateFields } from "./queryBuilder/UpdateFields";
-export { WhereFilter } from "./queryBuilder/WhereFilter";
-export { Query } from "./creators/classQuery";
-export { BasicQuery } from "./queryBuilder/BasicQuery";
-export { FromTables } from "./queryBuilder/FromTables";
-export { mqlFetchQuery } from "./functions/fetchQuery";
+import { JoinTables } from "./queryBuilder/JoinTables";
+import { SelectFields } from "./queryBuilder/SelectFields";
+import { UpdateFields } from "./queryBuilder/UpdateFields";
+import { WhereFilter } from "./queryBuilder/WhereFilter";
+import { Query } from "./creators/classQuery";
+import { BasicQuery } from "./queryBuilder/BasicQuery";
+import { FromTables } from "./queryBuilder/FromTables";
+import { mqlFetchQuery } from "./functions/fetchQuery";
+export { JoinTables, SelectFields, UpdateFields, WhereFilter, Query, BasicQuery, FromTables, mqlFetchQuery, };
